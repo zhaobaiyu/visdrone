853dc56b1c6b452d659d85164a04270a44130c6b
Baiyu Zhao <dev@baiyu.me>
2018-08-16 22:31:26 +0800

    continue pytorch implementation

--- pytorch/darknet.py
+++ pytorch/darknet.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 # from torch.autograd import Variable
 import numpy as np
+from util import *
 
 
 # parsing the configuration file
--- pytorch/util.py
+++ pytorch/util.py
@@ -4,3 +4,52 @@ import torch.nn.functional as F
 import numpy as np
 import cv2
 
+def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=True):
+    batch_size = prediction.size(0)
+    stride = inp_dim // prediction.size(2)
+    grid_size = inp_dim // stride
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+    
+    prediction = prediction.vew(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
+    prediction = prediction.transpose(1, 2).contigous()
+    prediction = prediction.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
+
+    anchors = [(a[0]/stride, a[1]/stride) for a anchors]
+    
+    # sigmoid the centre_x, centre_y, and object confidence
+    prediction[:, :, 0] = torch.sigmoid(prediction[:, :, 0])
+    prediction[:, :, 1] = torch.sigmoid(prediction[:, :, 1])
+    prediction[:, :, 4] = torch.sigmoid(prediction[:, :, 4])
+    
+    # add the center offsets
+    grid = np.arange(grid_size)
+    a, b = np.meshgrid(grid, grid)
+    
+    x_offset = torch.FloatTensor(a).view(-1, 1)
+    y_offset = torch.FloatTensor(b).view(-1, 1)
+    
+    if CUDA:
+        x_offset = x_offset.cuda()
+        y_offset = y_offset.cuda()
+        
+    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1, num_anchors).view(-1, 2).unsqueeze(0)
+    
+    prediction[:, :, :2] += x_y_offset
+    
+    # log space transform height and the width
+    anchors = torch.FloatTensor(anchors)
+    
+    if CUDA:
+        anchors = anchors.cuda()
+        
+    anchors = anchors.repeat(grid_size*grid_size, 1).unsqueeze(0)
+    prediction[:, :, 2:4] = torch.exp(prediction[:, :, 2:4])*anchors
+    
+    # sigmoid the class scores
+    prediction[:, :, 5: 5 + num_classes] = torch.sigmoid((prediction[:, :, 5: 5 + num_classes]))
+    
+    # resize the detection maps to the size of the input image
+    prediction[:, :, :4] *= stride
+    
+    return prediction
