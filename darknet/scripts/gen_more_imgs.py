93d603380feab181531970e272956bf32a1132c7
Baiyu Zhao <dev@baiyu.me>
2018-09-07 15:15:42 +0800

    fix file name error

--- darknet/scripts/gen_more_imgs.py
+++ darknet/scripts/gen_more_imgs.py
@@ -7,7 +7,7 @@ labels_path = os.path.join(os.getcwd(), 'labels')
 images = os.listdir(images_path)
 
 for image in images:
-    new_img_paths = crop_img(os.path.join(images_path, image), os.path.join(labels_path, image))
+    new_img_paths = crop_img(os.path.join(images_path, image), os.path.join(labels_path, image[:-4]+'.txt'))
     with open(os.path.join(os.getcwd(), 'images.txt'), 'a') as outfile:
         for path in new_img_paths:
             outfile.write(path + '\n')
